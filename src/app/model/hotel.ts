940a26ae607def1e500bcac4de93a4312aac5d66
rosana <rosmarielnunez@gmail.com>
2024-12-04 11:07:15 +0100

    Add updates

--- src/app/model/hotel.ts
+++ src/app/model/hotel.ts
@@ -2,8 +2,10 @@ export interface Hotel {
     id: string;
     name: string;
     image: string;
+    city: string;
     address: string;
     stars: number;
     rate: number;
     price: number;
+    description: string;
 }
